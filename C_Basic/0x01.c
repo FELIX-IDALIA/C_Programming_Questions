d51e65bc1dd4cf745e43de68784bbc4a2c2342b0
Felix Idalia <flxidalia@gmail.com>
2023-10-29 23:00:29 +0300

    File to print name, dob and mobile

---
+++ C_Basic/0x01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+	printf("Name   : Felix Idalia\n");
+	printf("DOB    : January, 01 1900\n");
+	printf("Mobile : 07000000 ");
+
+	return 0;
+}
